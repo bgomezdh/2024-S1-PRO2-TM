a56ccecd529985af7c7b045ca7e78e6e5610bea3
bgomezdh <bgomez@digitalhouse.com>
2024-05-06 14:19:24 -0300

    agregando proyecto con modelo de movies

--- clase5-sequelize/myapp/controllers/movieController.js
+++ clase5-sequelize/myapp/controllers/movieController.js
@@ -1,9 +1,17 @@
-const db = require('../db/data');
+/* const db = require('../db/data'); */ /* sacar */
+
+const db = require('../database/models');
 
 const indexController = {
     index: function(req, res) {
     
-      return res.render("movies", {listaPeliculas : db.lista}); 
+      db.Movie.findAll()
+      .then((result) => {
+        return res.render("movies", {listaPeliculas: result})
+      }).catch((err) => {
+         console.log(err);
+      });
+      
     },
     detalle: function(req, res) {
       let idPelicula = req.params.idPelicula;
